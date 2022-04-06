e7c8a9c09e21fbb4bb3749a4dcd28be66b0b28b4
Dallas Clymer <dallasclymer@gmail.com>
2022-04-06 06:43:34 -0400

    env config for prod deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:  {
+    apiKey: "AIzaSyCNCYdFIrbq9tOH13XUphT2ZfA-sloN1Ps",
+    authDomain: "aiod-3669a.firebaseapp.com",
+    databaseURL: "https://aiod-3669a-default-rtdb.firebaseio.com",
+    projectId: "aiod-3669a",
+    storageBucket: "aiod-3669a.appspot.com",
+    messagingSenderId: "1072368625341",
+    appId: "1:1072368625341:web:8e54d8e4550d9c7b6839d4",
+    measurementId: "G-WQZBQ1CWV6"
+  }
 };
